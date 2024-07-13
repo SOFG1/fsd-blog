7c5b80da2fa8776dbb5469afa812591aca352fe2
Eddy <edkosyan2000@gmail.com>
2024-07-13 17:57:06 +0400

    add nav in header

--- src/shared/ui/Header.tsx
+++ src/shared/ui/Header.tsx
@@ -1,22 +1,43 @@
-import { Link } from "react-router-dom"
-import styled from "styled-components"
+import { Link, NavLink } from "react-router-dom";
+import styled from "styled-components";
+import { routes } from "../constants/routes";
 
 const StyledHeader = styled.header`
-    background-color: orange;
-    padding: 20px;
-`
-
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: orange;
+  padding: 20px;
+`;
 
 const StyledLogo = styled(Link)`
-    color: greenyellow;
-    font-size: 30px;
-    font-weight: 700;
-    text-decoration: none;
-`
+  color: greenyellow;
+  font-size: 30px;
+  font-weight: 700;
+  text-decoration: none;
+`;
 
+const StyledNav = styled.nav`
+  display: flex;
+  gap: 30px;
+`;
+
+const StyledLink = styled(NavLink)`
+  color: #fff;
+  text-decoration: none;
+`;
+
+type RoutesKey = keyof typeof routes;
 
 export const Header = () => {
-    return <StyledHeader>
-        <StyledLogo to="/">Conduit</StyledLogo>
+  return (
+    <StyledHeader>
+      <StyledLogo to="/">Conduit</StyledLogo>
+      <StyledNav>
+        {Object.keys(routes).map((p) => (
+          <StyledLink to={routes[p as RoutesKey]}>{p}</StyledLink>
+        ))}
+      </StyledNav>
     </StyledHeader>
-}
+  );
+};
